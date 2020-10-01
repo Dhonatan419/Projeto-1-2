f771ef7f3cacc7f3ad979763fe964f80cd912228
Dhonatan419 <dhonatan_miranda@estudante.sc.senai.br>
2020-10-01 16:45:48 -0300

    first commit

---
+++ src/main/java/br/com/codificando/controller/indexController.java
@@ -0,0 +1,13 @@
+package br.com.codificando.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+	@GetMapping ("/")
+	public String chamarIndex() {
+		return "index";
+	}
+}
---
+++ src/main/java/br/com/codificando/repository/FuncionarioRepository.java
@@ -0,0 +1,15 @@
+package br.com.codificando.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.codificando.model.Funcionario;
+
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+	public List findAllByCargo(String cargo);
+	public List<Funcionario> findByCargoNot(String cargo);
+
+}
